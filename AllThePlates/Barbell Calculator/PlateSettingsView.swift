c95790c4f73406cb0d0951db4a0f4deee7013d20
Ryan Gross <ryan.gross1993@gmail.com>
2021-03-21 22:28:39 -0400

    Added PlateSettingsView, resolved compilation issue

---
+++ AllThePlates/Barbell Calculator/PlateSettingsView.swift
@@ -0,0 +1,15 @@
+//
+//  PlateSettings.swift
+//  AllThePlates
+//
+//  Created by Ryan Gross on 3/21/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct PlateSettingsView: View {
+    var body: some View {
+        Text("Plate Settings")
+    }
+}
