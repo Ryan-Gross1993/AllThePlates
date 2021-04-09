b7f5bc9de87de0e2722bc11112e536c290981a07
Ryan Gross <ryan.gross1993@gmail.com>
2021-04-08 21:22:06 -0400

    Initial commit for WatchKit impl

---
+++ ATPWatch Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ATPWatch Extension
+//
+//  Created by Ryan Gross on 4/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
