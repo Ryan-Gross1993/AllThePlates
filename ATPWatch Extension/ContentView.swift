fe063875788a41206cd0cddcfc4a1a77525c3577
Ryan Gross <ryan.gross1993@gmail.com>
2021-04-23 18:43:43 -0400

    Added settings view for watch, configured NavigationLink

--- ATPWatch Extension/ContentView.swift
+++ ATPWatch Extension/ContentView.swift
@@ -11,28 +11,30 @@ struct ContentView: View {
     @State var targetWgt = 145.0
     @State var barWgt = 45.0
     @State var plates = PlateUtility.generatePlates(qt: 6)
+    @State var isActive = false
     
     var body: some View {
-        VStack {
-            HStack(alignment: VerticalAlignment.center) {
-                VStack(alignment: HorizontalAlignment.leading) {
-                    Text("Bar: \(PlateUtility.format(self.barWgt))")
-                    Text("Target: \(PlateUtility.format(self.targetWgt))")
+        NavigationView {
+            VStack {
+                HStack(alignment: VerticalAlignment.center) {
+                    VStack(alignment: HorizontalAlignment.leading) {
+                        Text("Bar: \(PlateUtility.format(self.barWgt))")
+                        Text("Target: \(PlateUtility.format(self.targetWgt))")
+                    }
+                    Spacer()
+                    
+                    NavigationLink(destination: PlateSettingsView()) {
+                        Image(systemName: "slider.horizontal.3")
+                            .resizable()
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .frame(width: 30, height: 30, alignment: .center)
                 }
                 Spacer()
-                Button(action: {
-                    print("")
-                }, label: {
-                    Image(systemName: "slider.horizontal.3")
-                        .resizable()
-                })
-                .buttonStyle(PlainButtonStyle())
-                .frame(width: 30, height: 30, alignment: .center)
-            }
-            Spacer()
-            
-            if let neededPlates = PlateUtility.calculate(targetWgt: targetWgt, barWgt: barWgt, plates: plates) {
-                BarbellView(plates: neededPlates)
+                
+                if let neededPlates = PlateUtility.calculate(targetWgt: targetWgt, barWgt: barWgt, plates: plates) {
+                    BarbellView(plates: neededPlates)
+                }
             }
         }
     }
---
+++ ATPWatch Extension/PlateSettingsView.swift
@@ -0,0 +1,15 @@
+//
+//  PlateSettingsView.swift
+//  ATPWatch Extension
+//
+//  Created by Ryan Gross on 4/23/21.
+//
+
+import SwiftUI
+
+struct PlateSettingsView: View {
+    var body: some View {
+        Rectangle()
+            .foregroundColor(.red)
+    }
+}
