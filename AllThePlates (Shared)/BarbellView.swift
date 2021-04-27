fc43a0e9a7f6587b2e6f999039e8f2ba22b2607d
Ryan Gross <ryan.gross1993@gmail.com>
2021-04-27 18:09:51 -0400

    Added list toggle for watchOS barbell view

--- AllThePlates (Shared)/BarbellView.swift
+++ AllThePlates (Shared)/BarbellView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct BarbellView: View {
     @Binding var targetWgt: Double
     @Binding var barWgt: Double
+    @State var isList = false
     
     var body: some View {
         if let plates = PlateUtility.calculate(targetWgt: targetWgt, barWgt: barWgt, plates: PlateUtility.generatePlates(qt: 6)) {
@@ -43,33 +44,55 @@ struct BarbellView: View {
                     }
                 }
                 #else
-                Rectangle()
-                    .foregroundColor(.clear)
-                    .border(Color.white, width: 1.0)
-                
-                GeometryReader { proxy in
-                    let totalPlates = plates.map{return $0.qt}.reduce(0, +)
-                    let frame = proxy.frame(in: .local)
-                    let plateWidth = frame.width / 10
-                    let totalWidth = plateWidth * CGFloat(totalPlates) + CGFloat(totalPlates - 1) * 5
+                if isList {
+                    GeometryReader { proxy in
+                        let frame = proxy.frame(in: .local)
+
+                        VStack {
+                            ForEach(plates, id: \.self) { plate in
+                                Text("\(plate.qt)x @ \(PlateUtility.format(plate.wgt))")
+                            }
+                        }.onTapGesture {
+                            self.isList = toggleList()
+                        }
+                        .position(x: frame.midX, y: frame.midY)
+                    }
+                   
+                } else {
+                    Rectangle()
+                        .foregroundColor(.clear)
+                        .border(Color.white, width: 1.0)
                     
-                    HStack(spacing: 0) {
-                        ForEach(plates, id: \.self) { plate in
-                            ForEach(0..<plate.qt) { _ in
-                                PlateView(wgt: plate.wgt)
-                                    .frame(width: 20, height: frame.height * CGFloat(plate.getHeight()))
-                                
-                                Rectangle()
-                                    .foregroundColor(.gray)
-                                    .frame(width: 5, height: frame.height * 0.05)
+                    GeometryReader { proxy in
+                        let totalPlates = plates.map{return $0.qt}.reduce(0, +)
+                        let frame = proxy.frame(in: .local)
+                        let plateWidth = frame.width / 10
+                        let totalWidth = plateWidth * CGFloat(totalPlates) + CGFloat(totalPlates - 1) * 5
+                        
+                        HStack(spacing: 0) {
+                            ForEach(plates, id: \.self) { plate in
+                                ForEach(0..<plate.qt) { _ in
+                                    PlateView(wgt: plate.wgt)
+                                        .frame(width: 20, height: frame.height * CGFloat(plate.getHeight()))
+                                    
+                                    Rectangle()
+                                        .foregroundColor(.gray)
+                                        .frame(width: 5, height: frame.height * 0.05)
+                                }
                             }
                         }
+                        .frame(width: totalWidth, height: frame.height, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .position(x: frame.midX, y: frame.midY)
+                    }.onTapGesture {
+                        self.isList = toggleList()
                     }
-                    .frame(width: totalWidth, height: frame.height, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    .position(x: frame.midX, y: frame.midY)
                 }
                 #endif
             }
         }
+    }
+    
+    func toggleList() -> Bool {
+        return !isList
     }
 }
