da197da73136b714a36afe1985fac056a11ec5e0
Ryan Gross <ryan.gross1993@gmail.com>
2021-05-04 16:48:09 -0400

    Removed unused variable

--- ATPWatch Extension/ContentView.swift
+++ ATPWatch Extension/ContentView.swift
@@ -11,7 +11,6 @@ struct ContentView: View {
     @State var targetWgt = 145.0
     @State var barWgt = 45.0
     @State var plates = PlateUtility.generatePlates(qt: 6)
-    @State var isActive = false
     
     var body: some View {
         NavigationView {
