5e8b56a5aca63f580ea2e61ff12d85d8c63de870
Ryan Gross <ryan.gross1993@gmail.com>
2021-06-02 12:44:23 -0400

    Added AvailablePlatesView/ChangeWeightsView

---
+++ ATPWatch Extension/View/Settings/AvailablePlatesView.swift
@@ -0,0 +1,14 @@
+//
+//  AvailablePlatesView.swift
+//  ATPWatch Extension
+//
+//  Created by Ryan Gross on 6/2/21.
+//
+
+import SwiftUI
+
+struct AvailablePlatesView: View {
+    var body: some View {
+        EmptyView()
+    }
+}
---
+++ ATPWatch Extension/View/Settings/ChangeWeightsView.swift
@@ -0,0 +1,14 @@
+//
+//  ChangeWeightsView.swift
+//  ATPWatch Extension
+//
+//  Created by Ryan Gross on 6/2/21.
+//
+
+import SwiftUI
+
+struct ChangeWeightsView: View {
+    var body: some View {
+        EmptyView()
+    }
+}
